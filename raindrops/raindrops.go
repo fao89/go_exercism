f8c030380276e892cd432de6448a06a83c13d9dc
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-06-16 16:26:52 -0300

    Improve rain drops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -4,25 +4,22 @@ import "strconv"
 
 // Convert number to Raindrops
 func Convert(number int) string {
-	mod3 := number%3 == 0
-	mod5 := number%5 == 0
-	mod7 := number%7 == 0
-
-	if !mod3 && !mod5 && !mod7 {
-		return strconv.Itoa(number)
-	}
-
 	raindrops := ""
-	if mod3 {
+
+	if number%3 == 0 {
 		raindrops += "Pling"
 	}
 
-	if mod5 {
+	if number%5 == 0 {
 		raindrops += "Plang"
 	}
 
-	if mod7 {
+	if number%7 == 0 {
 		raindrops += "Plong"
+	}
+
+	if len(raindrops) == 0 {
+		return strconv.Itoa(number)
 	}
 
 	return raindrops
